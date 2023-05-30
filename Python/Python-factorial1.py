78cd57aa84fdc6b244fd9ee3f161b263e24d0fee
まつ <92625267+matsukz@users.noreply.github.com>
2023-05-30 13:54:17 +0900

    Update Python-factorial1.py
    開始・終了時間の表示に対応

--- Python/Python-factorial1.py
+++ Python/Python-factorial1.py
@@ -1,11 +1,15 @@
+import datetime
+
 Kazu = int(input("Enter..."))
 
 Ans = 0
 Flag = False
 
+stime = datetime.datetime.now().time()
+
 if Kazu == 1:
     Ans = 1
-    
+        
 else:
     while Kazu > 1:
         
@@ -15,12 +19,27 @@ else:
         else:
             Ans = Ans * (Kazu-1)
         Kazu = Kazu -1
+        #print(Kazu)
+        #try:
+            #Keika = open("Keika.txt", "a")
+            #File.write(str(Ans))
+            #Keika.write(f"{Ans:,}" + "\n")
+            #Keika.close()
+        #except exception as e:
+            #print("ERROR")
+
+etime = datetime.datetime.now().time()
 
 try:
     File = open("Ans.txt", "w")
+    File.write(str(stime) + "\n")
+    File.write(str(etime) + "\n")
     File.write(str(Ans))
     #File.write(f"{Ans:,}")
+
     File.close()
     print("OK!")
+
 except exception as e:
+
     print("ERROR")
